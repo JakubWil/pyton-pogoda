0a876c14fe6d0ed70c41137860efe799a8ee0538
Jakub Wilczek <jacob_0909@icloud.com>
2026-02-05 17:26:48 +0100

    add docker-compose, change main.py, add redis

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 import requests
 from dotenv import load_dotenv
 import os 
+import redis
 
 
 running = True
 
 load_dotenv()
+r = redis.Redis(host='cache', port=6379, decode_responses=True)
 key = os.environ.get("WEATHER_APP_KEY")
-location = "London,UK"
+location = os.environ.get("CITY_NAME", "Warsaw")
 
 
-while running:
-    location = input("Podaj nazwę miasta: ")
+
+def get_weather():
+    
+    cached_data = r.get(location)
+
+    if cached_data:
+        print(f"--- Dane pobrane z CACHE (Redis) dla: {location} ---")
+        print(cached_data)
+        return
+    
+    print(f"--- Brak danych w Cache. Pytam API dla: {location} ---")
 
     url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={key}'
-    x = requests.get(url)
 
-    if x.status_code == 200:
-        data = x.json()
+    try:
+        x = requests.get(url)
 
-        temp = data['currentConditions']['temp']
-        description = data['description']
-        address = data['resolvedAddress']
+        if x.status_code == 200:
+            data = x.json()
 
-        print(f"Miasto: {address}")
-        print(f"Temperatura: {temp}°C")
-        print(f"Opis: {description}")
+            temp = data['currentConditions']['temp']
+            description = data['description']
+            address = data['resolvedAddress']
+
+            r.set(f"{location}", temp)
+
+            print(f"Miasto: {address}")
+            print(f"Temperatura: {temp}°C")
+            print(f"Opis: {description}")
+    except Exception as e:
+        print(f"Wystąpił błąd połączenia: {e}") 
+
 
     
-    running = False
+if __name__ == "__main__":
+    get_weather()
 
 
 
