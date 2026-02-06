ea864449b3308c302e527d3dc7bd532d01ca40a8
Jakub Wilczek <jacob_0909@icloud.com>
2026-02-06 13:23:12 +0100

    Add features to main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 from dotenv import load_dotenv
 import os 
 import redis
+import json
+import re
+
 
 
 r = redis.Redis(host='cache', port=6379, decode_responses=True)
@@ -12,6 +15,34 @@ load_dotenv()
 key = os.environ.get("WEATHER_APP_KEY")
 location = os.environ.get("CITY_NAME", "Warsaw")
 
+def api_call(city, key):
+    url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={key}'
+
+    try:
+        x = requests.get(url)
+
+        if x.status_code == 200:
+            data = x.json()
+
+            cityValues = {
+                "temp": str(data['currentConditions']['temp']),
+                "description": data['description'],
+                "address": data['resolvedAddress']
+            }
+
+            print(f"Dane dla {city} pobrane z API.")
+            print(f"Temperatura: {cityValues['temp']}")
+            print(f"Opis: {cityValues['description']}")
+            print(f"Adres: {cityValues['address']}")
+
+            
+
+            r.set(f"{city}", json.dumps(cityValues))
+            print(f"Dane dla {city} zapisane w Cache (Redis).")
+
+           
+    except Exception as e:
+        print(f"Wystąpił błąd połączenia: {e}")
 
 
 def get_weather():
@@ -19,38 +50,21 @@ def get_weather():
     cities = location.split(",")
 
     for city in cities:
-    
-        cached_data = r.get(city)
 
-        print(cached_data)
+        if not re.match(r'^[a-zA-Z\s\-]+$', city):
+            print(f"--- Nieprawidłowa nazwa miasta: {city} ---")
+            continue
 
-        if cached_data:
+        if r.exists(city):
             print(f"--- Dane pobrane z CACHE (Redis) dla: {city} ---")
-            print(cached_data)
+            info = json.loads(r.get(city))
+            print(f"Temperatura: {info['temp']}")
+            print(f"Opis: {info['description']}")
+            print(f"Adres: {info['address']}")
             
-        else:
+        else:  
             print(f"--- Brak danych w Cache. Pytam API dla: {city} ---")
-
-            url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={key}'
-
-            try:
-                x = requests.get(url)
-
-                if x.status_code == 200:
-                    data = x.json()
-
-                    temp = data['currentConditions']['temp']
-                    description = data['description']
-                    address = data['resolvedAddress']
-
-                    r.set(f"{city}", temp)
-
-                    print(f"Miasto: {address}")
-                    print(f"Temperatura: {temp}°C")
-                    print(f"Opis: {description}")
-                    print("---------------")
-            except Exception as e:
-                print(f"Wystąpił błąd połączenia: {e}") 
+            api_call(city, key)
 
 
     
