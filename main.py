ea0034b32bc060d3aed437b289deacab602dc2af
Jakub Wilczek <jacob_0909@icloud.com>
2026-02-07 01:01:11 +0100

    Pytania

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ import re
 #Voluemn
 #Redis dashboard
 
+#1. Kiedy robić wirtualne środowisko, czy tylko jak pracuje lokalnie i sam w projekcie potrzebuje dane zalenznosci ? Czy robi sie takie cos ze venv wysyla na github zeby ktos inny tez mogl sie tam zalogowac i pracowac czy nie wiem wlasnie to jest to do Docker wyklucza, czy to i to mozna robic
+
+#2. Jak mam projekt, który ma docker to ja musze na tym kompie tez miec Docker Desktop zeby moc korzystac z tego czy jakbym w venv mial docker zainstalowany to bym nie musial, bo projekt teraz mi nie dziala na innym laptopie po tym jak zrobilem git clone i robie docker compose up i dostaje Cannot connect to the Docker daemon at unix:///Users/j.wilczek/.docker/run/docker.sock. Is the docker daemon running?
+
+#3. Co wlasnie z .env bo jesli w .gitignore daje .env to nie ma go na github, jakie sa bezpieczne metody przenoszenia .env powiedzmy ze jest tam klucz api i kazdy musi miec taki sam 
+
+#4. Potrzebuje nauki pracy z git, bo dalej widze ze mam braki, potrzebuje zebys mi opisal taki projektowy workflow jak to wyglada. Tez potrzebuje zebys mi powiedzial co dokladnie robi git clone, kolejna rzecz odnosnie autoryzacji ja tylko raz sie autoryzuje i to robie do github tak a potem jak chce zrobic push to po prostu musze zapisac w remote dane tego repo, ale jak np nie wiem teraz bede miec jedno repo na moim kompie ale np ktos zaprosi mnie do projektu to jak bedzie autoryzacja to nie do tego repo tylko do github zeby zweryfikowal ze to ja. No i to co jeszcze tak nie ogarniam to powiedzmy ze mam kilka kont na github to jak zrobic tak ze jak pracuje nad projektem A to tam mam wysylac z konta A , projekt B z konta B ? 
+
 
 r = redis.Redis(host='cache', port=6379, decode_responses=True)
 
